66adbe0698a38f13e713e32b265e842d2236205a
zubi1234 <48028473+zubi1234@users.noreply.github.com>
2019-03-13 22:22:02 -0700

    git commit

---
+++ UseCast.java
@@ -0,0 +1,9 @@
+public class UseCast {
+pulic static void main(String[] args) {
+int i;
+for(i=0; i<5; i++){
+System.out.println(i+"/3:" +i/3);
+System.out.println(i+"/3 with fraction:"+(double) i/3);
+System.out.println();
+}
+}
