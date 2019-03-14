8666609121ff78449f8e2d1d3edebdcddd73f1dd
zubi1234 <48028473+zubi1234@users.noreply.github.com>
2019-03-14 09:37:37 -0700

    git commit

---
+++ CastDemo.java
@@ -0,0 +1,21 @@
+class CastDemo {
+public static void main (String args[]) {
+public x ,y;
+byte b;
+int i;
+char ch;
+x=10.0;
+y=3.0;
+i =(int) (x/y);
+System.out.println("Integer outcome of x/y:" + i);
+i=100;
+b=(byte) i;
+System.out.println("Value of b:" +b);
+i=275
+b= (byte) i;
+System.out.println("Value of b: " + b);
+b=88;
+char= (char) b;
+System.out.println("ch: " + ch);
+}
+}
